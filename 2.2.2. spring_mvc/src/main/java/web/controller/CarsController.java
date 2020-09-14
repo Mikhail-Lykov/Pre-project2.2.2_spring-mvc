deebaca7f729bc8da8e3a4e587dc70fe1888b610
Mikhail-Lykov <Mikhail-Lykov@users.noreply.github.com>
2020-09-14 17:29:36 +0400

    commit

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 import web.service.CarServiceImp;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Controller
 public class CarsController {
 
